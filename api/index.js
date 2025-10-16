f344753eaaa6d47d9ed61eec1b3dbb65562bb346
Pham An Chi <chi.pa.devIT@gmail.com>
2025-10-16 09:16:55 +0700

    update deloy

---
+++ api/index.js
@@ -0,0 +1,8 @@
+export default function handler(req, res) {
+  res.json({ 
+    message: 'API is working!',
+    method: req.method,
+    url: req.url,
+    timestamp: new Date().toISOString()
+  });
+}
